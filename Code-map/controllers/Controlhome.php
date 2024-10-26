decd5d807e59cea7e509ecd3353c73034ee4dabe
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 21:03:47 +0200

    Main met body tags aan controllerhome toegevoegd

--- Code-map/controllers/Controlhome.php
+++ Code-map/controllers/Controlhome.php
@@ -2,6 +2,7 @@
 $paginatitel = "ProcodeX";
 
 require "../views/Layouts/Head.php";
+require "../views/Layouts/Main.html";
 require "../views/Layouts/Header.html";
 require "../views/Layouts/Homepage.html";
 require "../views/Layouts/Footer.html";
