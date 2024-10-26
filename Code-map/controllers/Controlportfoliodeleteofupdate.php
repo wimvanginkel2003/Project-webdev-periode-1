05e5bc6ee062277485d2819f6e0577271a64de64
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 23:35:36 +0200

    Project updaten of verwijderen uit portfolio

---
+++ Code-map/controllers/Controlportfoliodeleteofupdate.php
@@ -0,0 +1,10 @@
+<?php
+$paginatitel = "Project verwijderen of updaten admin| ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
+require "../views/Layouts/updateofdelete.html";
+require "../views/Layouts/Footer.html";
+
+?>
