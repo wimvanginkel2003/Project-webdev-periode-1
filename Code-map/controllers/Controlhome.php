cd8612b149d8d14a2f4b41dd1278bdd57bf1ef90
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 19:48:05 +0200

    Home main content toegevoegd aan home controller

--- Code-map/controllers/Controlhome.php
+++ Code-map/controllers/Controlhome.php
@@ -3,6 +3,7 @@ $paginatitel = "ProcodeX";
 
 require "../views/Layouts/Head.php";
 require "../views/Layouts/Header.html";
+require "../views/Layouts/Homepage.html";
 require "../views/Layouts/Footer.html";
 
 ?>
