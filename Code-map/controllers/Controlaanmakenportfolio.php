8fd1104b63d425c67ccc5b67c1eb3653f6122b3c
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 23:23:22 +0200

    Controller gemaakt voor aanmaken portfolio

---
+++ Code-map/controllers/Controlaanmakenportfolio.php
@@ -0,0 +1,10 @@
+<?php
+$paginatitel = "Project aanmaak beheer admin| ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
+require "../views/Layouts/Aanmakenportfolio.html";
+require "../views/Layouts/Footer.html";
+
+?>
