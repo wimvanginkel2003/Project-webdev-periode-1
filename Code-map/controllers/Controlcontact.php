671f36a619618ee20c8286bf80cb578bf8edd5f4
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 21:27:15 +0200

    Controller gemaakt en gecheckt

---
+++ Code-map/controllers/Controlcontact.php
@@ -0,0 +1,10 @@
+<?php
+$paginatitel = "Contact | ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
+require "../views/Layouts/Contact.html";
+require "../views/Layouts/Footer.html";
+
+?>
