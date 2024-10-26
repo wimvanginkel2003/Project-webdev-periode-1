3de5f95aa7de4e31e4b9c912c099d60aa8c82ad3
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 21:08:17 +0200

    Main met body tags aan controlleroverons toegevoegd

--- Code-map/controllers/Controloverons.php
+++ Code-map/controllers/Controloverons.php
@@ -3,6 +3,7 @@ $paginatitel = "Over ons | ProcodeX";
 
 require "../views/Layouts/Head.php";
 require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
 require "../views/Layouts/Overons.html";
 require "../views/Layouts/Footer.html";
 
