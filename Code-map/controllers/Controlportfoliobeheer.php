33805d3e24406bb4f3fc1dfa022b815b30791e8e
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 22:56:08 +0200

    Controller portfolio gecreerd

---
+++ Code-map/controllers/Controlportfoliobeheer.php
@@ -0,0 +1,10 @@
+<?php
+$paginatitel = "Portfolio beheer admin| ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
+require "../views/Layouts/Portfoliobeheer.html";
+require "../views/Layouts/Footer.html";
+
+?>
