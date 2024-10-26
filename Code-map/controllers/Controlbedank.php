17e1825c2118470610a3ed02c4cb7126649581bd
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 21:35:59 +0200

    bedank -t bijgewerkt

--- Code-map/controllers/Controlbedank.php
+++ Code-map/controllers/Controlbedank.php
@@ -4,7 +4,7 @@ $paginatitel = "We contacten je snel! | ProcodeX";
 require "../views/Layouts/Head.php";
 require "../views/Layouts/Header.html";
 require "../views/Layouts/Main.html";
-require "../views/Layouts/Contact.html";
+require "../views/Layouts/Bedank.html";
 require "../views/Layouts/Footer.html";
 
 ?>
