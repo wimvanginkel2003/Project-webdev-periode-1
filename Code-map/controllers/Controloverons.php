374316f22776f30b5f9a075413ee69d615fbc951
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 20:11:51 +0200

    Over ons view page HTML elementen met commentaar en controller file bijgewerkt

---
+++ Code-map/controllers/Controloverons.php
@@ -0,0 +1,9 @@
+<?php
+$paginatitel = "Over ons | ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Overons.html";
+require "../views/Layouts/Footer.html";
+
+?>
