5f4bf134418ae6e289b1033480338620fae7f434
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 22:17:04 +0200

    Controller pagina geordend

---
+++ Code-map/controllers/Controlportfolio.php
@@ -0,0 +1,10 @@
+<?php
+$paginatitel = "Portfolio | ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
+require "../views/Layouts/Portfolio.html";
+require "../views/Layouts/Footer.html";
+
+?>
