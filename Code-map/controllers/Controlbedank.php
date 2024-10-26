4529d1e111cced98d6f0b204ef7a1ef89566fa66
wim van ginkel <wim2003vanginkel@gmail.com>
2024-10-26 21:34:35 +0200

    Controller gemaakt bedank pagina

---
+++ Code-map/controllers/Controlbedank.php
@@ -0,0 +1,10 @@
+<?php
+$paginatitel = "We contacten je snel! | ProcodeX";
+
+require "../views/Layouts/Head.php";
+require "../views/Layouts/Header.html";
+require "../views/Layouts/Main.html";
+require "../views/Layouts/Contact.html";
+require "../views/Layouts/Footer.html";
+
+?>
