85e2a4ff361922829c0a48d94518582370030457
wim van ginkel <wim2003vanginkel@gmail.com>
2024-11-04 14:07:26 +0100

    database opgezet en verbinding gemaakt zodat contact gegevens in de database komen

--- Code-map/controllers/Controlcontact.php
+++ Code-map/controllers/Controlcontact.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 
 $paginatitel = "Contact | ProcodeX";
 
@@ -6,5 +7,28 @@ require __DIR__ . '/../views/Layouts/Head.php';
 require __DIR__ . '/../views/Layouts/Header.html';
 require __DIR__ . '/../views/Layouts/Contact.html';
 require __DIR__ . '/../views/Layouts/Footer.html';
+
+require __DIR__ . '/DBverbinding.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Naam = $_POST["Naam"];
+    $Email = $_POST["Email"];
+    $Telefoonnummer = $_POST["Telefoonnummer"];
+    $Aanvullende_informatie = $_POST["Aanvullende_info"];
+
+
+    $stmt = $conn->prepare("INSERT INTO ProcodeX_Contacten (Naam, Telefoonnummer, Email, Aanvullende_informatie) VALUES (:Naam, :Telefoonnummer, :Email, :Aanvullende_informatie)");
+    $stmt->bindParam(':Naam', $Naam);
+    $stmt->bindParam(':Email', $Email);
+    $stmt->bindParam(':Telefoonnummer', $Telefoonnummer);
+    $stmt->bindParam(':Aanvullende_informatie', $Aanvullende_informatie);
+
+    $stmt->execute();
+    header("Location: Controlbedank.php");
+    exit();
+
+}
+
+ob_end_flush();
 
 ?>
