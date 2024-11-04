e0c4057f398d76360c1561a401a510f2da5111f5
wim van ginkel <wim2003vanginkel@gmail.com>
2024-11-04 19:47:50 +0100

    commentaar toegevoegd aan javascript document

--- Code-map/views/Javscript/jsprojectwebdev.js
+++ Code-map/views/Javscript/jsprojectwebdev.js
@@ -1,3 +1,6 @@
+
+<!-- Dit script zorgt dat als iemand op het hamburger men klikt er een menu tevoorschijn komt -->
+
 function hamburgermenu() {
     var x = document.getElementById("IDUL");
     if (x.style.display === "block") {
